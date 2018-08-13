e26e77b098e0740fafc865a62aa2726823a97759
Josiah Maina <mainajosiahdev@gmail.com>
2018-08-13 07:44:48 +0300

    if for negeatives

--- js/master.js
+++ js/master.js
@@ -4,7 +4,16 @@ function userinput(){
   side1=parseInt(prompt("Enter the first side"));
   side2=parseInt(prompt("Enter the second side"));
   side3=parseInt(prompt("Enter the third side"));
-  result.push(side1,side2,side3)
+
+  if(side1===null || side2===null || side3===null){
+    alert("put valid length")
+  }
+  else if(side1<=0||side2<=0||side3<=0){
+    alert("ERROR !!!,NO TRIANGLE WITH A NEGATIVE LENGHT or ,0,")
+  }
+  else {
+    result.push(side1,side2,side3)
+  }
 }
 // function reloaads the page
 
@@ -13,16 +22,16 @@ function userinput(){
 // }
 // fuction that compares the sides
 function compare(){
-  if (side1===side2&&side2===side3){
+  if (result[0]===result[1]&&result[1]===result[2]){
     alert("This is an Equilateral Triangle"+"\n"+"The sides of the triangle are: "+result)
   }
-  else if (side1===side2|| side2===side3|| side1===side3) {
+  else if (result[0]===result[1]|| result[1]===result[2]|| result[0]===result[2]) {
     alert("This is an Isosceles Triangle"+"\n"+"The sides of the triangle are: "+result)
   }
-  else if((side1+side2)<=side3||(side2+side3<=side1)||(side1+side3<=side2)){
+  else if((result[0]+result[1])<=result[2]||(result[1]+result[2]<=result[0])||(result[0]+result[2]<=result[1])){
     alert("this is not a triangle"+"\n"+"The sides of the triangle are: "+result)
   }
-  else if ((side1+side2)>side3||(side2+side3)>side1||(side1+side3>side2)) {
+  else if ((result[0]+result[1])>result[2]||(result[1]+result[2])>result[0]||(result[0]+result[2]>result[1])) {
     alert("this is a Scalene triangle"+"\n"+"The sides of the triangle are: "+result)
   }
   else {
